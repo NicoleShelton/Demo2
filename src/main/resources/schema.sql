03717463cb55c707a56cb98936090656cff465b9
Nicole Hughes <n.shelton1106@gmail.com>
2018-08-07 13:55:40 -0500

    Added schema file

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,11 @@
+CREATE SEQUENCE item_id_seq;
+
+CREATE TABLE item (
+  item_id INTEGER(50) NOT NULL PRIMARY KEY,
+  csn VARCHAR(50) NOT NULL,
+  description VARCHAR(255),
+  create_user_id INTEGER(50) NOT NULL,
+  create_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  expire_user_id INTEGER(50),
+  expire_date TIMESTAMP
+);
