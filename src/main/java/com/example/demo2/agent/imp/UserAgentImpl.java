06f3724bb4376cf17237717ea33b77c75015dbd3
Nicole Hughes <n.shelton1106@gmail.com>
2018-08-07 10:53:12 -0500

    quick push

--- src/main/java/com/example/demo2/agent/imp/UserAgentImpl.java
+++ src/main/java/com/example/demo2/agent/imp/UserAgentImpl.java
@@ -2,22 +2,42 @@ package com.example.demo2.agent.imp;
 
 import com.example.demo2.agent.UserAgent;
 import com.example.demo2.api.UserController;
+import com.example.demo2.model.internal.User;
 import com.example.demo2.model.internal.request.GetUserRequest;
 import com.example.demo2.model.internal.response.GetUserResponse;
+import com.example.demo2.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import javax.annotation.Resource;
+
 @Service
 public class UserAgentImpl implements UserAgent{
     private static final Logger LOG = LoggerFactory.getLogger(UserAgentImpl.class);
+
+    @Resource
+    private UserService userService;
 
     @Override
     public GetUserResponse getUserById(GetUserRequest request) {
         final GetUserResponse response = new GetUserResponse();
 
         try {
+            final User user = userService.getUserById(request.getId());
+            if(user != null){
+                final GetUserResponse.User user = new GetUserResponse.User()
+                        .setFirstName(externalUser.getFirstName())
+                        .setLastName(externalUser.getLastName())
+                        .setId(externalUser.getId())
+                        .setJobTitle(externalUser.getJobTitle())
+                        .setLocationId(externalUser.getLocationId());
+                response.setUser(user);
+            }
+
+
+
             final RestTemplate restTemplate = new RestTemplate();
             final String url = "http://localhost:9999/user/get-by-id";
             final com.example.demo2.model.external.request.GetUserRequest externalGetUserRequest =
--- src/main/java/com/example/demo2/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/demo2/service/impl/UserServiceImpl.java
@@ -1,13 +1,37 @@
 package com.example.demo2.service.impl;
 
 import com.example.demo2.model.internal.User;
+import com.example.demo2.model.internal.response.GetUserResponse;
 import com.example.demo2.service.UserService;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class UserServiceImpl implements UserService {
     @Override
     public User getUserById(Long userId) {
-        return null;
+        final RestTemplate restTemplate = new RestTemplate();
+        final String url = "http://localhost:9999/user/get-by-id";
+        final com.example.demo2.model.external.request.GetUserRequest externalGetUserRequest =
+                new com.example.demo2.model.external.request.GetUserRequest().setId(userId);
+
+        final com.example.demo2.model.external.response.GetUserResponse externalGetUserResponse =
+                restTemplate.postForObject(url, externalGetUserRequest, com.example.demo2.model.external.response.GetUserResponse.class);
+
+        final User user;
+        if(externalGetUserResponse != null) {
+            final com.example.demo2.model.external.response.GetUserResponse.User externalUser =
+                    externalGetUserResponse.getUser();
+
+            user = new User()
+                    .setFirstName(externalUser.getFirstName())
+                    .setLastName(externalUser.getLastName())
+                    .setId(externalUser.getId())
+                    .setJobTitle(externalUser.getJobTitle())
+                    .setLocationId(externalUser.getLocationId());
+        } else {
+            user = null;
+        }
+        return user;
     }
 }
