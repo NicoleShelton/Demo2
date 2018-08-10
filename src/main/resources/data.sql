206378e5c500191d8462986e05fb01577a934eb1
Nicole Hughes <n.shelton1106@gmail.com>
2018-08-10 10:24:29 -0500

    Added dummy data for phones

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -13,8 +13,71 @@ INSERT INTO item (
   'IPH10-256SG',
   'iPhone X 256GB Space Gray',
   1,
-  2,
+  3,
   SYSDATE + 1,
-  1,
-  12
+  2,
+  1
+);
+
+INSERT INTO item (
+  item_id,
+  csn,
+  description,
+  create_user_id,
+  expire_user_id,
+  expire_date,
+  serial_item_id,
+  serial_number,
+) VALUES (
+  (SELECT item_id_seq.NEXTVAL
+   FROM dual),
+  'IPH8-168RG',
+  'iPhone 8 168GB Rose Gold',
+  2,
+  3,
+  SYSDATE + 1,
+  3,
+  2
+);
+
+INSERT INTO item (
+  item_id,
+  csn,
+  description,
+  create_user_id,
+  expire_user_id,
+  expire_date,
+  serial_item_id,
+  serial_number,
+) VALUES (
+  (SELECT item_id_seq.NEXTVAL
+   FROM dual),
+  'SAMN9-128LP',
+  'Samsung Galaxy Note 9 128GB Lavender Purple',
+  3,
+  3,
+  SYSDATE + 1,
+  4,
+  3
+);
+
+INSERT INTO item (
+  item_id,
+  csn,
+  description,
+  create_user_id,
+  expire_user_id,
+  expire_date,
+  serial_item_id,
+  serial_number,
+) VALUES (
+  (SELECT item_id_seq.NEXTVAL
+   FROM dual),
+  'SAMS9-64B',
+  'Samsung Galaxy S9+ 64GB Black',
+  4,
+  3,
+  SYSDATE + 1,
+  5,
+  4
 );
